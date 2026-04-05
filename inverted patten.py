3311cc42e4b5fa2da9ac428fa31788dd761f9542
lovely joshi <lovelyjoshi7002@gmail.com>
2026-04-05 11:38:50 +0530

    first commit

---
+++ inverted patten.py
@@ -0,0 +1,6 @@
+#inverted pattern 
+n=5
+for i in range(n,0,-1):
+    for j in range(1,i+1):
+        print("*",end=" ")
+    print()
---
+++ minimum.py
@@ -0,0 +1,6 @@
+n=[5,3,8,9,1,2,4,5,6]
+min=n[0]
+for i in range(len(n)):
+   if n[i]<min:
+      min=n[i]
+print("minimum number",min)      
