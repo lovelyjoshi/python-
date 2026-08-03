a79a65e866bec38bbf77a2ede4708f664dd10352
lovely joshi <lovelyjoshi7002@gmail.com>
2026-08-03 20:38:03 +0530

    if elif statement

---
+++ marks.py
@@ -0,0 +1,9 @@
+marks=45
+if marks>=90:
+    print("grade A")
+elif marks>=75 and marks<90:
+    print("grade B")
+elif marks>=50 and marks<75:
+    print("grade C")
+else:
+    print("fail")          
