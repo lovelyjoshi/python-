cdccc67df213d348393ff70624c38dab2a84e8bb
lovely joshi <lovelyjoshi7002@gmail.com>
2026-09-14 00:54:33 +0530

    basic string function

---
+++ basic.py
@@ -0,0 +1,31 @@
+# Create a string
+name = "Lovely"
+
+# Print string
+print(name)
+
+# Length of string
+print(len(name))
+
+# Access characters
+print(name[0])      # L
+print(name[-1])     # y
+
+# String slicing
+print(name[0:3])    # Lov
+
+# Convert to uppercase/lowercase
+print(name.upper())
+print(name.lower())
+
+# Replace text
+print(name.replace("Lovely", "Joshi"))
+
+# Concatenation
+first = "Hello"
+second = "World"
+print(first + " " + second)
+
+# Check string
+text = "Python"
+print("Py" in text)  # True
